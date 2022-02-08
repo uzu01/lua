f647180b5eb66d354966c12b88f9ea5238fbd60d
uzu01 <77998551+uzu01@users.noreply.github.com>
2022-02-08 17:09:05 +0800

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,25 @@
+if game.GameId == 2655311011 then -- AD
+	a = "https://pastebin.com/raw/hjM3yGEx"
+elseif game.PlaceId == 8448735476 then -- DBZ
+	a = "https://pastebin.com/raw/cv40V2AF"
+elseif game.PlaceId == 8592863835 then -- MG2
+	a = "https://pastebin.com/raw/udNsHfKw"
+elseif game.PlaceId == 6329844902 then -- LP
+	a = "https://pastebin.com/raw/UVxJyWaV"
+elseif game.PlaceId == 8140820363 then -- AH
+	a = "https://pastebin.com/raw/5xxXpMUv"
+elseif game.PlaceId == 7560156054 then -- CS
+	a = "https://pastebin.com/raw/XS1WGkCs"
+elseif game.PlaceId == 8649665676 then -- APS New World
+	a = "https://pastebin.com/raw/NcyduV34"
+elseif game.PlaceId == 7549520141 then -- A Power S
+	a = "https://pastebin.com/raw/rATuq79t"
+elseif game.PlaceId == then -- WPS
+	a = "https://pastebin.com/raw/Bd6R0GaC"
+elseif game.PlaceId == then -- APS
+	a = "https://pastebin.com/raw/XuEH0Sb8"
+elseif game.PlaceId == then -- FS
+	a = "https://pastebin.com/raw/NTmmwqHj"
+end
+
+loadstring(game:HttpGet(a))()
