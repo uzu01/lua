173a1b582b101036455ac6384f321a12a0c1c316
uzu01 <77998551+uzu01@users.noreply.github.com>
2022-02-08 17:10:59 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,24 +1,24 @@
-if game.GameId == 2655311011 then -- AD
+if game.GameId == 2655311011 then -- Anime Dimension
 	a = "https://pastebin.com/raw/hjM3yGEx"
-elseif game.PlaceId == 8448735476 then -- DBZ
+elseif game.PlaceId == 8448735476 then -- DBZ Unleashed
 	a = "https://pastebin.com/raw/cv40V2AF"
-elseif game.PlaceId == 8592863835 then -- MG2
+elseif game.PlaceId == 8592863835 then -- Magnet Simulator 2
 	a = "https://pastebin.com/raw/udNsHfKw"
-elseif game.PlaceId == 6329844902 then -- LP
+elseif game.PlaceId == 6329844902 then -- Last Pirates
 	a = "https://pastebin.com/raw/UVxJyWaV"
-elseif game.PlaceId == 8140820363 then -- AH
+elseif game.PlaceId == 8140820363 then -- Anime Heroes
 	a = "https://pastebin.com/raw/5xxXpMUv"
-elseif game.PlaceId == 7560156054 then -- CS
+elseif game.PlaceId == 7560156054 then -- Clicker Simulator
 	a = "https://pastebin.com/raw/XS1WGkCs"
-elseif game.PlaceId == 8649665676 then -- APS New World
+elseif game.PlaceId == 8649665676 then -- Anime Punching Simulator New World
 	a = "https://pastebin.com/raw/NcyduV34"
-elseif game.PlaceId == 7549520141 then -- A Power S
+elseif game.PlaceId == 7549520141 then -- Anime Power Simulator
 	a = "https://pastebin.com/raw/rATuq79t"
-elseif game.PlaceId == then -- WPS
+elseif game.PlaceId == 8554378337 then -- Weapon Fighting Simulator
 	a = "https://pastebin.com/raw/Bd6R0GaC"
-elseif game.PlaceId == then -- APS
+elseif game.PlaceId == then -- Anime Punching Simulator
 	a = "https://pastebin.com/raw/XuEH0Sb8"
-elseif game.PlaceId == then -- FS
+elseif game.PlaceId == then -- Fish Sim
 	a = "https://pastebin.com/raw/NTmmwqHj"
 end
 
