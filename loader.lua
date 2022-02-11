a52920270e8cfda55b1f05f5cc9ac8a83b988c24
uzu01 <77998551+uzu01@users.noreply.github.com>
2022-02-11 11:58:53 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,29 +1,27 @@
-if game.GameId == 2655311011 then -- Anime Dimension
-	a = "https://pastebin.com/raw/hjM3yGEx"
-elseif game.PlaceId == 8448735476 then -- DBZ Unleashed
-	a = "https://pastebin.com/raw/cv40V2AF"
-elseif game.PlaceId == 8592863835 then -- Magnet Simulator 2
-	a = "https://pastebin.com/raw/udNsHfKw"
-elseif game.PlaceId == 6329844902 then -- Last Pirates
-	a = "https://pastebin.com/raw/UVxJyWaV"
-elseif game.PlaceId == 8140820363 then -- Anime Heroes
-	a = "https://pastebin.com/raw/5xxXpMUv"
-elseif game.PlaceId == 7560156054 then -- Clicker Simulator
-	a = "https://pastebin.com/raw/XS1WGkCs"
-elseif game.PlaceId == 8649665676 then -- Anime Punching Simulator New World
-	a = "https://pastebin.com/raw/NcyduV34"
-elseif game.PlaceId == 7549520141 then -- Anime Power Simulator
-	a = "https://pastebin.com/raw/rATuq79t"
-elseif game.PlaceId == 8554378337 then -- Weapon Fighting Simulator
-	a = "https://scriptblox.com/raw/WFS-or-Autofarm_393"
-elseif game.PlaceId == 8357510970 then -- Anime Punching Simulator
-	a = "https://pastebin.com/raw/XuEH0Sb8"
-elseif game.PlaceId == 7363858705 then -- Fish Sim
-	a = "https://pastebin.com/raw/NTmmwqHj"
-elseif game.PlaceId == 7842028704 then -- Anime Clash Simulator
-	a = "https://pastebin.com/raw/1uGavJHw"
-elseif game.PlaceId == 7989049516 then -- Anime Masters
-	a = "https://pastebin.com/raw/T11XNtVb"
+if game.GameId == 2655311011 then
+	a = "https://raw.githubusercontent.com/uzu01/lua/main/games/Anime%20Dimension.lua" -- Anime Dimension
+elseif game.PlaceId == 8448735476 then
+	a = "https://raw.githubusercontent.com/uzu01/lua/main/games/DBZ%20Unleashed.lua" -- DBZ Unleashed
+elseif game.PlaceId == 8592863835 then
+	a = "https://raw.githubusercontent.com/uzu01/lua/main/games/Magnet%20Simulator%202.lua" -- Magnet Simulator 2
+elseif game.PlaceId == 6329844902 then
+	a = "https://raw.githubusercontent.com/uzu01/lua/main/games/Last%20Pirates.lua" -- Last Pirates
+elseif game.PlaceId == 8140820363 then
+	a = "https://raw.githubusercontent.com/uzu01/lua/main/games/Anime%20Heroes%20.lua" -- Anime Heroes
+elseif game.PlaceId == 7560156054 then
+	a = "https://raw.githubusercontent.com/uzu01/lua/main/games/Clicker%20Simulator.lua" -- Clicker Simulator
+elseif game.PlaceId == 8649665676 then
+	a = "https://raw.githubusercontent.com/uzu01/lua/main/games/Anime%20Punching%20Simulator%20New%20World.lua" -- Anime Punching Simulator New World
+elseif game.PlaceId == 7549520141 then
+	a = "https://raw.githubusercontent.com/uzu01/lua/main/games/Anime%20Power%20Simulator.lua"-- Anime Power Simulator
+elseif game.PlaceId == 8554378337 then
+	a = "https://raw.githubusercontent.com/uzu01/lua/main/games/Weapon%20Fighting%20Simulator.lua" -- Weapon Fighting Simulator
+elseif game.PlaceId == 8357510970 then
+	a = "https://raw.githubusercontent.com/uzu01/lua/main/games/Anime%20Punching%20Simulator.lua" -- Anime Punching Simulator
+elseif game.PlaceId == 7363858705 then
+	a = "https://raw.githubusercontent.com/uzu01/lua/main/games/Fish%20Sim.lua" -- Fish Sim
+elseif game.PlaceId == 7989049516 then
+	a = "https://raw.githubusercontent.com/uzu01/lua/main/games/Anime%20Masters.lua" -- Anime Masters
 end
 
 loadstring(game:HttpGet(a))()
