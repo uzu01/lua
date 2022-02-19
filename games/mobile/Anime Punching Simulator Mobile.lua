55755dbda40a2a130d9fc986b935ff2ebebd3fc7
uzu01 <77998551+uzu01@users.noreply.github.com>
2022-02-19 21:43:21 +0800

    Update Anime Punching Simulator Mobile.lua

--- games/mobile/Anime Punching Simulator Mobile.lua
+++ games/mobile/Anime Punching Simulator Mobile.lua
@@ -1,7 +1,6 @@
 repeat wait() until game:IsLoaded()
 
 game:GetService("Players").LocalPlayer.Idled:Connect(function()
-    game:GetService("VirtualUser"):CaptureController()
     game:GetService("VirtualUser"):ClickButton2(Vector2.new())
 end)
 
@@ -20,11 +19,10 @@ if game.PlaceId == 8357510970 then
         autoMaxElite = false;
     }
 
-	local codes = {
-        "HUNTER";
-        "70KFAVS";
-        "45KLIKES";
-        "16MVISITS";
+    local codes = {
+        "14MVISITS";
+        "Dreamzinho";
+        "40KLIKES";
         "HERO";
         "35KLIKES";
         "50KFAVS";
@@ -55,9 +53,9 @@ if game.PlaceId == 8357510970 then
 
     local plr = game.Players.LocalPlayer.Character.HumanoidRootPart
     local tickk = tick()
-    local reb = {}
-    local practiceArea = {}
-    local egg = {}
+    local reb = {"-- Rebirth Amount --"}
+    local practiceArea = {"-- Practice Area --"}
+    local egg = {"-- Select Egg --"}
 
     for i, v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Ui.CenterFrame.Rebirths.Frame:GetChildren()) do
         if v:IsA("ImageLabel") and not table.find(reb,v.Price.Text) then
@@ -77,315 +75,190 @@ if game.PlaceId == 8357510970 then
         end
     end
 
-    function noclip()
-        for i, v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
-            if v:IsA("BasePart") and v.CanCollide == true then
-                v.CanCollide = false
+    local library = loadstring(game:HttpGet("https://pastebin.com/raw/Uz6HijUN", true))()
+    local b = library:CreateWindow("Farming")
+    local c = library:CreateWindow("Heroes")
+    local d = library:CreateWindow("Shop")
+    local e = library:CreateWindow("Teleport")
+    local f = library:CreateWindow("Misc")
+
+    b:Toggle("Auto Tap", {flag = "a"}, function(v)
+        _G.Settings.autoTap = v
+
+        task.spawn(function()
+            while task.wait() do
+                if not _G.Settings.autoTap then break end
+                game:GetService("ReplicatedStorage").Remotes.TappingEvent:FireServer()
+            end
+        end)
+    end)
+
+    b:Toggle("Auto Rebirth", {flag = "b"}, function(v)
+        _G.Settings.autoRebirth = v
+
+        task.spawn(function()
+            while task.wait() do
+                if not _G.Settings.autoRebirth then break end
+                game:GetService("ReplicatedStorage").Remotes.ClientRemote:InvokeServer("Rebirths",selectedNum)
+            end
+        end)
+    end)
+
+    b:Dropdown("Select Rebirth Amount", {flag = "c", list = reb}, function(value) 
+        for i, v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Ui.CenterFrame.Rebirths.Frame:GetChildren()) do
+            if v:IsA("ImageLabel") and v.Price.Text == tostring(value) then
+                selectedNum = tonumber(v.Name)
             end
         end
-    end
-    
-    function teleport(mob)
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = mob.CFrame
-    end
-    
-    local library = loadstring(game:HttpGetAsync("https://pastebin.com/raw/znibQh36"))()
-    local MainWindow = library:CreateWindow("Main")
-    local TeleWindow = library:CreateWindow("Teleport")
-    local MiscWindow = library:CreateWindow("Misc")
-    local FarmFolder = MainWindow:AddFolder("Farming")
-    local HeroFolder = MainWindow:AddFolder("Heroes")
-    local ShopFolder = MainWindow:AddFolder("Shop")
+    end)
 
-    FarmFolder:AddToggle({
-        text = "Auto Tap", 
-        state = false,
-        callback = function(v) 
-            _G.Settings.autoTap = v
+    b:Toggle("Auto Practice", {flag = "d"}, function(v)
+        _G.Settings.autoPractice = v
 
-            task.spawn(function()
-                while task.wait() do
-                    if not _G.Settings.autoTap then break end
-                    game:GetService("ReplicatedStorage").Remotes.TappingEvent:FireServer()
-                end
-            end)
-        end
-    })
-
-    FarmFolder:AddToggle({
-        text = "Auto Rebirth", 
-        state = false,
-        callback = function(v) 
-            _G.Settings.autoRebirth = v
-
-            task.spawn(function()
-                while task.wait() do
-                    if not _G.Settings.autoRebirth then break end
-                    game:GetService("ReplicatedStorage").Remotes.ClientRemote:InvokeServer("Rebirths",selectedNum)
-                end
-            end)
-        end
-    })
-
-    FarmFolder:AddList({
-        text = "Select Rebirth Amount", 
-        values = reb, 
-        callback = function(value) 
-            for i, v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Ui.CenterFrame.Rebirths.Frame:GetChildren()) do
-                if v:IsA("ImageLabel") and v.Price.Text == tostring(value) then
-                    selectedNum = tonumber(v.Name)
+        task.spawn(function()
+            while task.wait() do
+                if not _G.Settings.autoPractice then break end
+                for i, v in pairs(game.Workspace.__SETTINGS.__INTERACT:GetChildren()) do
+                    if v.Name == "Practice" and v.Boost.Value == tonumber(selectedArea) then
+                        plr.CFrame = v.CFrame * CFrame.new(0,0,5)
+                        game:GetService("ReplicatedStorage").Remotes.ClientRemote:InvokeServer("Practice",v)
+                    end
                 end
             end
-        end
-    })
+        end)
+    end)
 
-    FarmFolder:AddToggle({
-        text = "Auto Practice", 
-        state = false,
-        callback = function(v) 
-            _G.Settings.autoPractice = v
-            
-            task.spawn(function()
-                while task.wait() do
-                    if not _G.Settings.autoPractice then break end
-                    for i, v in pairs(game.Workspace.__SETTINGS.__INTERACT:GetChildren()) do
-                        if v.Name == "Practice" and v.Boost.Value == tonumber(selectedArea) then
-                            pcall(function() 
-                                repeat task.wait()
-                                    teleport(v)
-                                    game:GetService('VirtualInputManager'):SendKeyEvent(true, "E", false, game) task.wait()
-                                    game:GetService('VirtualInputManager'):SendKeyEvent(false, "E", false, game)
-                                until not _G.Settings.autoPractice
-                            end)
-                        end
+    b:Dropdown("Select Practice Area", {flag = "e", list = practiceArea}, function(v) 
+        selectedArea = string.match(v,"%d+")
+    end)
+
+    c:Toggle("Open Egg", {flag = "f"}, function(v)
+        _G.Settings.autoEgg = v
+
+        task.spawn(function()
+            while task.wait() do
+                if not _G.Settings.autoEgg then break end
+                plr.CFrame = selectedEgg.CFrame * CFrame.new(0,0,5)
+                game:GetService("ReplicatedStorage").Remotes.ClientRemote:InvokeServer("EGG",selectedEgg,_G.Settings.tripleEgg,{})
+            end
+        end)
+    end)
+
+	c:Toggle("Triple Egg",{flag = "z"}, function(v)
+		if c.flags.z then
+			_G.Settings.tripleEgg = "3"
+		else
+			_G.Settings.tripleEgg = "1"
+		end
+	end)
+
+    c:Dropdown("Select Egg", {flag = "g", list = egg}, function(value) 
+        for i, v in pairs(game.Workspace.__SETTINGS.__INTERACT:GetChildren()) do
+            if v.Name == "EGG" and v.Tier.Value == value then 
+                selectedEgg = v
+            end
+        end 
+    end)
+
+    c:Toggle("Auto Max Elite", {flag = "asd"}, function(v)
+        _G.Settings.autoMaxElite = v
+
+        task.spawn(function()
+            while task.wait() do
+                if not _G.Settings.autoMaxElite then break end
+                game:GetService("ReplicatedStorage").Remotes.NPCSystem:InvokeServer(true,"MAXELITE")
+            end
+        end)
+    end)
+
+    d:Toggle("Buy Upgrade", {flag = "h"}, function(v)
+        _G.Settings.autoUpgrade = v
+
+        task.spawn(function()
+            while task.wait() do
+                if not _G.Settings.autoUpgrade then break end
+                for i, v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Ui.CenterFrame.Shop.Upgrades:GetChildren()) do
+                    if v:IsA("ImageLabel") then
+                        game:GetService("ReplicatedStorage").Remotes.ClientRemote:InvokeServer("Upgrade",tonumber(v.Name))
                     end
                 end
-            end)
-        end
-    })
 
-    FarmFolder:AddList({
-        text = "Select Practice Area", 
-        values = practiceArea, 
-        callback = function(v) 
-            selectedArea = string.match(v,"%d+")
-        end
-    })
+            end
+        end)
+    end)
 
-    HeroFolder:AddToggle({
-        text = "Auto Open Egg", 
-        state = false,
-        callback = function(v) 
-            _G.Settings.autoEgg = v
+    d:Toggle("Buy Rank", {flag = "i"}, function(v)
+        _G.Settings.autoRank = v
 
-            task.spawn(function()
-                while task.wait() do
-                    if not _G.Settings.autoEgg then break end
-                    pcall(function() 
-                        teleport(selectedEgg)
-                        repeat task.wait()
-                            game:GetService('VirtualInputManager'):SendKeyEvent(true, "E", false, game) task.wait()
-                            game:GetService('VirtualInputManager'):SendKeyEvent(false, "E", false, game)
-                            if _G.Settings.tripleEgg == "1" then
-                                firesignal(game.Players.LocalPlayer.PlayerGui.Ui.CenterFrame.BuyEgg.Frame.Buy1.Button.MouseButton1Click)
-                            elseif _G.Settings.tripleEgg == "2" then
-                                firesignal(game.Players.LocalPlayer.PlayerGui.Ui.CenterFrame.BuyEgg.Frame.Buy3.Button.MouseButton1Click)
-                            end
-                        until not _G.Settings.autoEgg
-                    end)
-                end
-            end)
-        end
-    })
-
-    HeroFolder:AddToggle({
-        text = "Triple Egg", 
-        state = false,
-        callback = function(v) 
-            lulul = v
-            
-            if lulul then
-                _G.Settings.tripleEgg = "2"
-            else
-                _G.Settings.tripleEgg = "1"
-            end 
-        end
-    })
-
-    HeroFolder:AddList({
-        text = "Select Egg", 
-        values = egg, 
-        callback = function(value) 
-            for i, v in pairs(game.Workspace.__SETTINGS.__INTERACT:GetChildren()) do
-                if v.Name == "EGG" and v.Tier.Value == value then 
-                    selectedEgg = v
-                end
-            end  
-        end
-    })
-
-    HeroFolder:AddToggle({
-        text = "Auto Max Elite", 
-        state = false,
-        callback = function(v) 
-            _G.Settings.autoMaxElite = v
-
-            task.spawn(function()
-                while task.wait() do
-                    if not _G.Settings.autoMaxElite then break end
-                    game:GetService("ReplicatedStorage").Remotes.NPCSystem:InvokeServer(true,"MAXELITE")
-                end
-            end)
-        end
-    })
-
-    ShopFolder:AddToggle({
-        text = "Auto Buy Upgrade", 
-        state = false,
-        callback = function(v) 
-            _G.Settings.autoUpgrade = v
-
-            task.spawn(function()
-                while task.wait() do
-                    if not _G.Settings.autoUpgrade then break end
-                    for i, v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Ui.CenterFrame.Shop.Upgrades:GetChildren()) do
-                        if v:IsA("ImageLabel") then
-                            game:GetService("ReplicatedStorage").Remotes.ClientRemote:InvokeServer("Upgrade",tonumber(v.Name))
-                        end
-                    end
-                    
-                end
-            end)
-        end
-    })
-
-    ShopFolder:AddToggle({
-        text = "Auto Buy Rank", 
-        state = false,
-        callback = function(v) 
-            _G.Settings.autoRank = v
-
-            task.spawn(function()
-                while task.wait() do
-                    if not _G.Settings.autoRank then break end
-                    for i, v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Ui.CenterFrame.Shop.Ranks:GetChildren()) do
-                        if v:IsA("ImageLabel") then
-                            game:GetService("ReplicatedStorage").Remotes.ClientRemote:InvokeServer("Ranks",tonumber(v.Name))
-                        end
-                    end
-                    
-                end
-            end)
-        end
-    })
-
-    ShopFolder:AddToggle({
-        text = "Auto Buy Aura", 
-        state = false,
-        callback = function(v) 
-            _G.Settings.autoAura = v
-
-            task.spawn(function()
-                while task.wait() do
-                    if not _G.Settings.autoAura then break end
-                    for i, v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Ui.CenterFrame.Auras.Frame:GetChildren()) do
-                        if v:IsA("ImageLabel") then
-                            game:GetService("ReplicatedStorage").Remotes.ClientRemote:InvokeServer("Auras",tonumber(v.Name))
-                        end
-                    end
-                    
-                end
-            end)
-        end
-    })
-
-    ShopFolder:AddToggle({
-        text = "Auto Buy World", 
-        state = false,
-        callback = function(value) 
-            _G.Settings.autoWorld = value
-
-            task.spawn(function()
-                while task.wait() do
-                    if not _G.Settings.autoWorld then break end
-                    for i, v in pairs(game:GetService("Workspace")["__SETTINGS"]["__AREAS"]:GetChildren()) do
-                        game:GetService("ReplicatedStorage").Remotes.ClientRemote:InvokeServer("Areas",v.Name)
+        task.spawn(function()
+            while task.wait() do
+                if not _G.Settings.autoRank then break end
+                for i, v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Ui.CenterFrame.Shop.Ranks:GetChildren()) do
+                    if v:IsA("ImageLabel") then
+                        game:GetService("ReplicatedStorage").Remotes.ClientRemote:InvokeServer("Ranks",tonumber(v.Name))
                     end
                 end
-            end)
-        end
-    })
+
+            end
+        end)
+    end)
+
+    d:Toggle("Buy Aura", {flag = "j"}, function(v)
+        _G.Settings.autoAura = v
+
+        task.spawn(function()
+            while task.wait() do
+                if not _G.Settings.autoAura then break end
+                for i, v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Ui.CenterFrame.Auras.Frame:GetChildren()) do
+                    if v:IsA("ImageLabel") then
+                        game:GetService("ReplicatedStorage").Remotes.ClientRemote:InvokeServer("Auras",tonumber(v.Name))
+                    end
+                end
+
+            end
+        end)
+    end)
+
+    d:Toggle("Buy World", {flag = "asd"}, function(value)
+        _G.Settings.autoWorld = value
+
+        task.spawn(function()
+            while task.wait() do
+                if not _G.Settings.autoWorld then break end
+                for i, v in pairs(game:GetService("Workspace")["__SETTINGS"]["__AREAS"]:GetChildren()) do
+                    game:GetService("ReplicatedStorage").Remotes.ClientRemote:InvokeServer("Areas",v.Name)
+                end
+            end
+        end)
+    end)
 
     for i, v in pairs(game:GetService("Workspace")["__SETTINGS"]["__AREAS"]:GetChildren()) do
-        TeleWindow:AddButton({
-            text = v.Name,
-            callback = function()
-                teleport(v)
-            end
-        })
+        e:Button(v.Name, function()
+            plr.CFrame = v.CFrame
+        end)
     end
 
-    MiscWindow:AddToggle({
-        text = "Claim Group Reward", 
-        state = false,
-        callback = function(v) 
-            _G.Settings.autoClaim = v
-
-            task.spawn(function()
-                while task.wait() do
-                    if not _G.Settings.autoClaim then break end
-                    game:GetService("ReplicatedStorage").Remotes.ClientRemote:InvokeServer("GroupChest")
-                end
-            end)
-        end
-    })
-
-    MiscWindow:AddButton({
-        text = "Tp to Second World",
-        callback = function()
-            for i, v in pairs(game.Workspace.__Map:GetChildren()) do
-                if v:FindFirstChild("TouchInterest") then
-                    firetouchinterest(v,plr,0)
-                end
+    f:Button("New World",function()
+        for i, v in pairs(game.Workspace.__Map:GetChildren()) do
+            if v:FindFirstChild("TouchInterest") then
+                firetouchinterest(v,plr,0)
             end
         end
-    })
+    end)
 
-    MiscWindow:AddButton({
-        text = "Redeem Codes",
-        callback = function()
-            for i, v in pairs(codes) do
-                game:GetService("ReplicatedStorage").Remotes.ClientRemote:InvokeServer("Codes",v)
-            end
+    f:Button("Redeem Codes", function()
+        for i, v in pairs(codes) do
+            game:GetService("ReplicatedStorage").Remotes.ClientRemote:InvokeServer("Codes",v)
         end
-    })
+    end)
 
-    MiscWindow:AddBind({
-        text = "Toggle GUI", 
-        key = "LeftControl", 
-        callback = function() 
-            library:Close()
-        end
-    })
+    f:Section("Script by Uzu")
 
-    MiscWindow:AddButton({
-        text = "Script by Uzu",
-        callback = function()
-            print("a")
-        end
-    })
+	f:Button("discord.gg/waAsQFwcBn", function()
+		setclipboard(tostring("discord.gg/waAsQFwcBn"))
+	end)
 
-    MiscWindow:AddButton({
-        text = "discord.gg/waAsQFwcBn",
-        callback = function()
-            setclipboard("discord.gg/waAsQFwcBn")
-        end
-    })
-
-    library:Init()
-    
-	warn("Last Update: 2/8/22, 7:23 PM PST")
-    warn("script took ",string.format("%.0f",tick()-tickk),"seconds to load")
+    warn("Script loaded in",string.format("%.0f",tick()-tickk),"seconds")
 
 elseif game.PlaceId == 8649665676 then
 
