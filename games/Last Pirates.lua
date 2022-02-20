d80c439a29dfe24aedd9b703d4a9129e991629a2
uzu01 <77998551+uzu01@users.noreply.github.com>
2022-02-20 11:39:36 +0800

    Update Last Pirates.lua

--- games/Last Pirates.lua
+++ games/Last Pirates.lua
@@ -1,21 +1,29 @@
+
 repeat wait() until game:IsLoaded()
 
 game:GetService("Players").LocalPlayer.Idled:Connect(function()
     game:GetService("VirtualUser"):ClickButton2(Vector2.new())
 end)
 
-_G.autofarm = false
-_G.autoSkill = false
-_G.Melee = false
-_G.Sword = false
-_G.Defense = false
-_G.Devil = false
-_G.autoget = false
+_G.Settings = {
+    autofarm = false,
+    selectedMob = "Bandit [Lv:5]",
+    selectedTool = "Combat",
+    autoskill = false,
+    melee = false,
+    sword = false,
+    defense = false,
+    devil = false,
+    autoget = false,
+}
 
-local selectedMob = "Bandit [Lv:5]"
-local selectedTool = "Combat"
+local Player = game:GetService("Players").LocalPlayer
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local VirtualUser = game:GetService("VirtualUser")
+local VirtualInputManager = game:GetService("VirtualInputManager")
 local mob = {}
 local tool = {}
+
 local keys = {
     "Z";
     "X";
@@ -36,267 +44,315 @@ for i, v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
 end
 
 function click()
-    game:GetService("VirtualUser"):CaptureController()
-    game:GetService("VirtualUser"):Button1Down(Vector2.new(69, 69))		
+    VirtualUser:CaptureController()
+    VirtualUser:Button1Down(Vector2.new(69, 69))		
 end
 
 function equipTool()
-	game.Players.LocalPlayer.Character.Humanoid:EquipTool(game.Players.LocalPlayer.Backpack:FindFirstChild(selectedTool))
+	Player.Character.Humanoid:EquipTool(Player.Backpack:FindFirstChild(_G.Settings.selectedTool))
 end
 
 function startQuest()
     task.spawn(function()
-        if game:GetService("Players").LocalPlayer.PlayerGui.QuestGui.Enabled == false then
-            lol = string.split(selectedMob," [")
-            game:GetService("ReplicatedStorage").FuncQuest:InvokeServer(lol[1])
+        if Player.PlayerGui.QuestGui.Enabled == false then
+            lol = string.split(_G.Settings.selectedMob," [")
+            ReplicatedStorage.FuncQuest:InvokeServer(lol[1])
         end
     end)
 end
 
 function autoHaki()
-    if not game.Players.LocalPlayer.Character:FindFirstChild("Buso") then
-        game:GetService("ReplicatedStorage").Haki:FireServer("Buso")
-        game:GetService("ReplicatedStorage").HakiRemote:FireServer("Ken")
+    if not Player.Character:FindFirstChild("Buso") then
+        ReplicatedStorage.Haki:FireServer("Buso")
+        ReplicatedStorage.HakiRemote:FireServer("Ken")
     end
 end
 
 function useSkill()
-    task.spawn(function()
-        local VIM = game:GetService('VirtualInputManager')
-
-        for i, v in pairs(keys) do
-            VIM:SendKeyEvent(true, v, false, game) task.wait()
-            VIM:SendKeyEvent(true, v, false, game)
-        end
-    end)
+    for i, v in pairs(keys) do
+        VirtualInputManager:SendKeyEvent(true, v, false, game) 
+        task.wait()
+        VirtualInputManager:SendKeyEvent(false, v, false, game)
+    end
 end
 
-local library = loadstring(game:HttpGet("https://pastebin.com/raw/Uz6HijUN", true))()
-local w = library:CreateWindow("Farming")
-local b = library:CreateWindow("Stats")
-local c = library:CreateWindow("Shop")
-local d = library:CreateWindow("Teleports")
-local e = library:CreateWindow("Misc")
+local library = loadstring(game:HttpGet"https://pastebin.com/raw/CNw4eMqu")()
+local w = library:Window("Uzu Scripts", "Last Pirates", Color3.fromRGB(66, 134, 245), Enum.KeyCode.LeftControl)
+local HomeTab = w:Tab("Home", 6026568198)
 
-w:Toggle("Auto Farm", {flag = "a"}, function(a)
-	_G.autofarm = a
-	
-	task.spawn(function()
-		while task.wait() do
-			if not _G.autofarm then break end
-			pcall(function()
-				local plr = game.Players.LocalPlayer.Character.HumanoidRootPart
-				startQuest()
+HomeTab:Button("Updates:", "", function()
+    library:Notification("[+] New Gui", "Thanks")
+end)
+
+HomeTab:Line()
+
+HomeTab:Button("Discord"," ", function()
+    setclipboard("discord.gg/waAsQFwcBn")
+    library:Notification("discord.gg/waAsQFwcBn", "Alright")
+end)
+
+HomeTab:Button("Script by Uzu"," ", function()
+    setclipboard("discord.gg/waAsQFwcBn")
+    library:Notification("discord.gg/waAsQFwcBn", "Alright")
+end)
+
+local FarmingTab = w:Tab("Farming", 6034287535)
+
+FarmingTab:Toggle("Auto Farm", "", false, function(t)
+    _G.Settings.autofarm = t
+
+    task.spawn(function()
+        while task.wait() do
+            if not _G.Settings.autofarm then break end
+            pcall(function()
+                local plr = Player.Character.HumanoidRootPart
+                startQuest()
                 autoHaki()
-				for i, v in pairs(game.Workspace.Lives:GetChildren()) do
-					if v.Name == selectedMob and v:FindFirstChild("Humanoid") then
+                for i, v in pairs(game.Workspace.Lives:GetChildren()) do
+                    if v.Name == _G.Settings.selectedMob and v:FindFirstChild("Humanoid") then
                         plr.CFrame = v.HumanoidRootPart.CFrame * CFrame.new(0,7,0) * CFrame.Angles(math.rad(-90),0,0)
                         equipTool()
                         click()
-					end
-				end
-			end)
-		end
-	end)
+                    end
+                end
+            end)
+        end
+    end)
 end)
 
-w:Dropdown("Select Mob", { flag = "b", list = mob}, function(a)
-	selectedMob = a
+local mobDrop = FarmingTab:Dropdown("Select Mob", mob, function(v)
+    _G.Settings.selectedMob = v
 end)
 
-w:Button("Refresh Mob", function()
-    table.clear(mob)
+FarmingTab:Button("Refresh Mob"," ", function()
+    mobDrop:Clear()
+
+    local mob = {}
+
     for i, v in pairs(game:GetService("Workspace").Lives:GetChildren()) do
         if v:IsA("Model") and v:FindFirstChild("Folder") and not table.find(mob,v.Name) then
             table.insert(mob,v.Name)
         end
     end
+
+    for i, v in pairs(mob) do
+        mobDrop:Add(v)
+    end
 end)
 
-w:Dropdown("Select Tool", { flag = "b", list = tool}, function(a)
-    selectedTool = a
+local toolDrop = FarmingTab:Dropdown("Select Tool", tool, function(v)
+    _G.Settings.selectedTool = v
 end)
 
-w:Button("Refresh Tool", function()
-    table.clear(tool)
-    for i, v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+FarmingTab:Button("Refresh Tool"," ", function()
+    toolDrop:Clear()
+
+    local tool = {}
+
+    for i, v in pairs(Player.Backpack:GetChildren()) do
         if v:IsA("Tool") then
             table.insert(tool,v.Name)
         end
     end
+
+    for i, v in pairs(tool) do
+        toolDrop:Add(v)
+    end
 end)
 
-w:Toggle("Auto Skill", {flag = "v"}, function(a)
-    _G.autoSkill = a
+FarmingTab:Toggle("Auto Skill", "", false, function(t)
+    _G.Settings.autoskill = t
 
     task.spawn(function()
         while task.wait() do
-            if not _G.autoSkill then break end
+            if not _G.Settings.autoskill then break end
             useSkill()
         end
     end)
 end)
 
-b:Toggle("Melee",{flag = b}, function(a)
-	_G.Melee = a
-	
-	task.spawn(function()
-		while task.wait() do
-			if not _G.Melee then break end
-			game:GetService("ReplicatedStorage").okStats:FireServer(1,"1")
-		end
-	end)
+local StatsTab = w:Tab("Stats", 6035078901)
+
+StatsTab:Toggle("Melee", "", false, function(t)
+    _G.Settings.melee = t
+
+    task.spawn(function()
+        while task.wait() do
+            if not _G.Settings.melee then break end
+            ReplicatedStorage.okStats:FireServer(1,"1")
+        end
+    end)
 end)
 
-b:Toggle("Sword",{flag = b}, function(a)
-	_G.Sword = a
-	
-	task.spawn(function()
-		while task.wait() do
-			if not _G.Sword then break end
-			game:GetService("ReplicatedStorage").okStats:FireServer(1,"2")
-		end
-	end)
+StatsTab:Toggle("Sword", "", false, function(t)
+    _G.Settings.sword = t
+
+    task.spawn(function()
+        while task.wait() do
+            if not _G.Settings.sword then break end
+            ReplicatedStorage.okStats:FireServer(1,"2")
+        end
+    end)
 end)
 
-b:Toggle("Defense",{flag = b}, function(a)
-	_G.Defense = a
-	
-	task.spawn(function()
-		while task.wait() do
-			if not _G.Defense then break end
-			game:GetService("ReplicatedStorage").okStats:FireServer(1,"3")
-		end
-	end)
+StatsTab:Toggle("Defense", "", false, function(t)
+    _G.Settings.defense = t
+
+    task.spawn(function()
+        while task.wait() do
+            if not _G.Settings.defense then break end
+            ReplicatedStorage.okStats:FireServer(1,"3")
+        end
+    end)
 end)
 
-b:Toggle("Devil Fruit",{flag = b}, function(a)
-	_G.Devil = a
-	
-	task.spawn(function()
-		while task.wait() do
-			if not _G.Devil then break end
-			game:GetService("ReplicatedStorage").okStats:FireServer(1,"4")
-		end
-	end)
+StatsTab:Toggle("Devil Fruit", "", false, function(t)
+    _G.Settings.devil = t
+
+    task.spawn(function()
+        while task.wait() do
+            if not _G.Settings.devil then break end
+            ReplicatedStorage.okStats:FireServer(1,"4")
+        end
+    end)
 end)
 
-c:Button("Buso Haki", function()
-    local plr = game.Players.LocalPlayer.Character.HumanoidRootPart
+local shopTab = w:Tab("Shop", 6031265987)
+
+shopTab:Button("Buso Haki"," ", function()
+    local plr = Player.Character.HumanoidRootPart
     plr.CFrame = game:GetService("Workspace").HakiSeller.AAA.BusoHaki.CFrame
 end)
 
-c:Button("Ken Haki", function()
-    local plr = game.Players.LocalPlayer.Character.HumanoidRootPart
+shopTab:Button("Ken Haki"," ", function()
+    local plr = Player.Character.HumanoidRootPart
     plr.CFrame = CFrame.new(-6278.6826171875, 32.993167877197, 3832.9084472656)
 end)
 
-c:Button("Random Color Haki", function()
-    local plr = game.Players.LocalPlayer.Character.HumanoidRootPart
+shopTab:Button("Random Color Haki"," ", function()
+    local plr = Player.Character.HumanoidRootPart
     plr.CFrame = game:GetService("Workspace")["Random Color haki"]["Random Color haki"].Head.CFrame
 end)
 
-c:Button("Random Fruit", function()
-    local plr = game.Players.LocalPlayer.Character.HumanoidRootPart
+shopTab:Button("Random Fruit"," ", function()
+    local plr = Player.Character.HumanoidRootPart
     plr.CFrame = game:GetService("Workspace").RandomFruit.HumanoidRootPart.CFrame
 end)
 
-c:Button("Black Leg", function()
-    local plr = game.Players.LocalPlayer.Character.HumanoidRootPart
+shopTab:Button("Black Leg"," ", function()
+    local plr = Player.Character.HumanoidRootPart
     plr.CFrame = game:GetService("Workspace").Blackleg.Click.CFrame
 end)
 
-c:Button("Pole", function()
-    local plr = game.Players.LocalPlayer.Character.HumanoidRootPart
+shopTab:Button("Pole"," ", function()
+    local plr = Player.Character.HumanoidRootPart
     plr.CFrame = game:GetService("Workspace")["Pole Seller"].PoleClick.CFrame
 end)
 
-c:Button("Cutlass", function()
-    local plr = game.Players.LocalPlayer.Character.HumanoidRootPart
+shopTab:Button("Cutlass"," ", function()
+    local plr = Player.Character.HumanoidRootPart
     plr.CFrame = game:GetService("Workspace").cutlass.Click.CFrame
 end)
 
-c:Button("Bisento", function()
-    local plr = game.Players.LocalPlayer.Character.HumanoidRootPart
+shopTab:Button("Bisento"," ", function()
+    local plr = Player.Character.HumanoidRootPart
     plr.CFrame = game:GetService("Workspace").Bisento.Part.CFrame
 end)
 
-c:Button("Bisento v2", function()
-    local plr = game.Players.LocalPlayer.Character.HumanoidRootPart
+shopTab:Button("Bisento v2"," ", function()
+    local plr = Player.Character.HumanoidRootPart
     plr.CFrame = game:GetService("Workspace")["BisenV2 NPC"].Model.Model.BisenV2["Right Leg"].CFrame
 end)
 
-c:Button("Saber", function()
-    local plr = game.Players.LocalPlayer.Character.HumanoidRootPart
+shopTab:Button("Saber"," ", function()
+    local plr = Player.Character.HumanoidRootPart
     plr.CFrame = CFrame.new(3138.58984375, 71.283683776855, -2338.1533203125)
 end)
 
-c:Button("Shisui", function()
-    local plr = game.Players.LocalPlayer.Character.HumanoidRootPart
+shopTab:Button("Shisui"," ", function()
+    local plr = Player.Character.HumanoidRootPart
     plr.CFrame = game:GetService("Workspace").MISC.Handle.CFrame
 end)
 
-c:Button("Katana", function()
-    local plr = game.Players.LocalPlayer.Character.HumanoidRootPart
+shopTab:Button("Katana"," ", function()
+    local plr = Player.Character.HumanoidRootPart
     plr.CFrame = game:GetService("Workspace").KatanaShop.KatanaShop.Head.CFrame
 end)
 
-d:Button("Factory", function()
-    local plr = game.Players.LocalPlayer.Character.HumanoidRootPart
+local teleTab = w:Tab("Teleports", 6035202055)
+
+teleTab:Button("Factory"," ", function()
+    local plr = Player.Character.HumanoidRootPart
     plr.CFrame = game:GetService("Workspace").Factory.Block.CFrame
 end)
 
 for i, v in pairs(game:GetService("Workspace")["Spawn island"]:GetChildren()) do
-    d:Button(v.Name,function()
-        local plr = game.Players.LocalPlayer.Character.HumanoidRootPart
+    teleTab:Button(v.Name," ", function()
+        local plr = Player.Character.HumanoidRootPart
         plr.CFrame = v.CFrame
     end)
 end
 
-e:Toggle("Auto Get Fruit", {flag = "a"}, function(v)
-    _G.autoget = v
-    
+local MiscTab = w:Tab("Misc", 6031215984)
+
+MiscTab:Toggle("Auto Get Devil Fruit", "", false, function(t)
+    _G.Settings.autoget = t
+
     task.spawn(function()
         while task.wait() do
-            if not _G.autoget then break end
+            if not _G.Settings.autoget then break end
             for i, v in pairs(game.Workspace.Maps:GetDescendants()) do
                 if v.Name == "ProximityPrompt" then
-                    local plr = game.Players.LocalPlayer.Character.HumanoidRootPart
+                    local plr = Player.Character.HumanoidRootPart
                     plr.CFrame = v.Parent.CFrame    
                     fireproximityprompt(v)
                 end
             end
-        end 
+        end
     end)
 end)
 
-e:Button("Inventory", function()
-    local plr = game.Players.LocalPlayer.Character.HumanoidRootPart
+MiscTab:Button("Inventory"," ", function()
+    local plr = Player.Character.HumanoidRootPart
     plr.CFrame = CFrame.new(413.05569458008, 40.559078216553, -1830.5759277344)
 end)
 
-e:Button("Serverhop", function()
+MiscTab:Line()
+
+MiscTab:Slider("Walk Speed", "",18, 100, 0, function(v)
+    Player.Character.Humanoid.WalkSpeed = v
+end)
+
+MiscTab:Slider("Jump Height", "", 7.5, 100, 0, function(v)
+    Player.Character.Humanoid.JumpHeight = v
+end)
+
+MiscTab:Line()
+
+MiscTab:Button("Rejoin", "", function()
+    game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId,game.JobId,game.Players.LocalPlayer)
+end)
+
+MiscTab:Button("Serverhop", "", function()
     while task.wait() do
         local Servers = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. game.PlaceId .. '/servers/Public?sortOrder=Asc&limit=100'))
         for i,v in pairs(Servers.data) do
             if v.id ~= game.JobId then
-                wait()
+                task.wait()
                 game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, v.id)
             end
         end
     end
 end)
 
-e:Button("Script by Uzu", function()
-    print('a')
-end)
-
-e:Button("Discord",function()
-    setclipboard(tostring("discord.gg/waAsQFwcBn"))
-end)
-
-for i, v in pairs(game:GetService("Players").LocalPlayer.GPOwned:GetChildren()) do
-    if v:IsA("BoolValue") then
-        v.Value = true
+MiscTab:Button("Serverhop Low Server", "", function()
+    while task.wait() do
+        local Servers = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. game.PlaceId .. '/servers/Public?sortOrder=Asc&limit=100'))
+        for i,v in pairs(Servers.data) do
+            if v.id ~= game.JobId and v.playing <= 3 then
+                task.wait()
+                game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, v.id)
+            end
+        end
     end
-end
+end)
