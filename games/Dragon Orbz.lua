ca44f8a9c5061ea21f4b3008c25a197701688c31
uzu01 <77998551+uzu01@users.noreply.github.com>
2022-02-21 18:09:15 +0800

    Update Dragon Orbz.lua

--- games/Dragon Orbz.lua
+++ games/Dragon Orbz.lua
@@ -49,7 +49,7 @@ game:GetService("RunService").Stepped:Connect(function()
     end
 end)
 
-local library = loadstring(game:HttpGet"https://pastebin.com/raw/CNw4eMqu")()
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/uzu01/lua/main/ui/flux.lua"))()
 local w = library:Window("Uzu Scripts", "v1.0.0", Color3.fromRGB(66, 134, 245), Enum.KeyCode.RightControl)
 
 local HomeTab = w:Tab("Home", 6026568198)
