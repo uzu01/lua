547d8dc33a41e27e7f19ce77fb65ba469d1bdd23
uzu01 <77998551+uzu01@users.noreply.github.com>
2022-02-26 14:39:32 +0800

    Update Rap Simulator.lua

--- games/Rap Simulator.lua
+++ games/Rap Simulator.lua
@@ -4,10 +4,13 @@ game:GetService("Players").LocalPlayer.Idled:Connect(function()
     game:GetService("VirtualUser"):ClickButton2(Vector2.new())
 end)
  
-local player =      game:GetService("Players").LocalPlayer
-local area =        player.Area
-local fire =        player.Fire
-local fire_space =  player.FireSpace
+local Player = game:GetService("Players").LocalPlayer
+local VirtualInputManager = game:GetService("VirtualInputManager")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local area = Player.Area
+local fire = Player.Fire
+local fire_space = Player.FireSpace
 local selected_producer = 1
 local selected_computer = 1
 local prod = {}
@@ -27,16 +30,6 @@ end
 
 table.sort(prod, function(a,b) return string.match(a,"%d") < string.match(b,"%d") end)
 table.sort(comp, function(a,b) return string.match(a,"%d") < string.match(b,"%d") end)
-
-function mic()
-    local asd
-    for i, v in pairs(game:GetService("Workspace").Studio.Items:GetChildren()) do
-        if string.match(v.Name,"MIC") then
-            asd = v.ID.Value
-        end
-    end
-    return asd
-end
  
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/uzu01/lua/main/ui/wallyv2.lua", true))()
 local w = library:CreateWindow("Farming")
@@ -54,29 +47,43 @@ w:Toggle("Full Auto Farm", {flag = "toggle1"}, function(v)
                 -- Mic Thingy??
                 if area.Value == "Stage" and fire.Value < fire_space.Value then 
                     wait(1.5)
-                    game:GetService("ReplicatedStorage").Remotes.RequestEquip:FireServer()
+                    ReplicatedStorage.Remotes.RequestEquip:FireServer()
                     repeat task.wait()
-                        game:GetService("ReplicatedStorage").Remotes.Flick:FireServer()
+                        VirtualInputManager:SendKeyEvent(true, "E", false, game)
+                        VirtualInputManager:SendKeyEvent(false, "E", false, game)
+                        task.wait()
                     until fire.Value == fire_space.Value or not _G.autofarm
                 end
                 -- Rap thingy??
                 if area.Value == "House" then 
                     wait(1.5)
+                    for i, v in pairs(game:GetService("Workspace").Studio.Items:GetChildren()) do
+                        if string.match(v.Name,"MIC") then
+                            Player.Character.HumanoidRootPart.CFrame = v.BasePart.CFrame * CFrame.new(0,10,0)
+                        end
+                    end
                     repeat task.wait()
-                        game:GetService("ReplicatedStorage").Remotes.Flick:FireServer(tonumber(mic()))
+                        for i, v in pairs(Player.PlayerGui:GetChildren()) do
+                            if v:FindFirstChild("Rap") then 
+                                v.Rap.float.click:Fire()
+                            end
+                        end
+                        VirtualInputManager:SendKeyEvent(true, "E", false, game)
+                        VirtualInputManager:SendKeyEvent(false, "E", false, game)
+                        task.wait()
                     until fire.Value == 0 or not _G.autofarm
                     wait(1.5)
-                    game:GetService("ReplicatedStorage").Remotes.ToStage:FireServer()
+                    ReplicatedStorage.Remotes.ToStage:FireServer()
                 end
                 -- Tele to House
                 if fire.Value == fire_space.Value and area.Value ~= "House" then
                     wait(1.5)
-                    game:GetService("ReplicatedStorage").Remotes.ToHouse:FireServer()
+                    ReplicatedStorage.Remotes.ToHouse:FireServer()
                 end
                 -- Tele to Stage
                 if fire.Value < fire_space.Value and area.Value == "Lobby" then
                     wait(1.5)
-                    game:GetService("ReplicatedStorage").Remotes.ToStage:FireServer()
+                    ReplicatedStorage.Remotes.ToStage:FireServer()
                 end
             end)
         end
@@ -90,8 +97,8 @@ w:Toggle("Auto Get Trash", {flag = "a"}, function(v)
         while task.wait(.2) do
             if not _G.trash then break end
             for _, v in pairs({"Bottle", "Water Bottle", "Solo Cup"}) do
-                game:GetService("ReplicatedStorage").Remotes.PickUpItem:FireServer(v)
-                game:GetService("ReplicatedStorage").Remotes.TrashItem:FireServer(v)
+                ReplicatedStorage.Remotes.PickUpItem:FireServer(v)
+                ReplicatedStorage.Remotes.TrashItem:FireServer(v)
             end
         end
     end)
@@ -105,7 +112,7 @@ c:Toggle("Buy Stand", {flag = "a"}, function(v)
             if not _G.autobuy then break end
             -- Buy Stand
             for i, v in pairs(game:GetService("Workspace").UpgradeInt.Stand:GetChildren()) do
-                game:GetService("ReplicatedStorage").Remotes.BuyStand:FireServer(tonumber(v.Name))
+                ReplicatedStorage.Remotes.BuyStand:FireServer(tonumber(v.Name))
             end
         end
     end)
@@ -119,7 +126,7 @@ c:Toggle("Buy Mic", {flag = "a"}, function(v)
             if not _G.autobuy2 then break end
             -- Buy Held
             for i, v in pairs(game:GetService("Workspace").UpgradeInt.Held:GetChildren()) do
-                game:GetService("ReplicatedStorage").Remotes.BuyHeld:FireServer(tonumber(v.Name))
+                ReplicatedStorage.Remotes.BuyHeld:FireServer(tonumber(v.Name))
             end
         end
     end)
@@ -131,8 +138,8 @@ d:Toggle("Producer Chest", {flag = "a"}, function(v)
     task.spawn(function()
         while task.wait() do
             if not open_producer then break end
-            game:GetService("ReplicatedStorage").Remotes.RequestSpin:FireServer(tonumber(selected_producer),false)
-            game:GetService("ReplicatedStorage").Remotes.SpinFinish:FireServer()
+            ReplicatedStorage.Remotes.RequestSpin:FireServer(tonumber(selected_producer),false)
+            ReplicatedStorage.Remotes.SpinFinish:FireServer()
         end
     end)
 end)
@@ -147,8 +154,8 @@ d:Toggle("Computer Chest", {flag = "a"}, function(v)
     task.spawn(function()
         while task.wait() do
             if not open_computer then break end
-            game:GetService("ReplicatedStorage").Remotes.RequestSpin:FireServer(tonumber(selected_computer),true)
-            game:GetService("ReplicatedStorage").Remotes.SpinFinish:FireServer()
+            ReplicatedStorage.Remotes.RequestSpin:FireServer(tonumber(selected_computer),true)
+            ReplicatedStorage.Remotes.SpinFinish:FireServer()
         end
     end)
 end)
@@ -157,11 +164,11 @@ d:Dropdown("Select Chest", {flag = "a", list = comp}, function(v)
     selected_computer = string.match(v,"%d+")
 end)
 
-for i, v in pairs(require(game:GetService("ReplicatedStorage").Modules.Towns)) do
+for i, v in pairs(require(ReplicatedStorage.Modules.Towns)) do
     for a, b in pairs(v) do
         if a == "Name" then
             e:Button(b, function()
-                game:GetService("ReplicatedStorage").Remotes.Fly:FireServer(b)
+                ReplicatedStorage.Remotes.Fly:FireServer(b)
             end)
         end
     end
