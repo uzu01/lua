4ebfdb1ea7fe3a4e0231659f16998875b8995341
uzu01 <77998551+uzu01@users.noreply.github.com>
2022-04-04 23:22:24 -0700

    Update Pepsi.lua

--- ui/Pepsi.lua
+++ ui/Pepsi.lua
@@ -3320,7 +3320,7 @@ function library:CreateWindow(options, ...)
 					end
 				end)
 				library.signals[1 + #library.signals] = userInputService.InputChanged:Connect(function(input)
-					if not library.colorpicker and sliderDragging and input.UserInputType == Enum.UserInputType.MouseMovement then
+					if not library.colorpicker and sliderDragging and input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
 						sliding(input, sliderInner, sliderColored)
 					end
 				end)
